55a391ca3d70484fb9ebff20ce0ee34e8911ee38
nithinreddy8811 <nithinreddy8811@gmail.com>
2025-06-24 15:46:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "amazon_linux_vm" {
-  ami                         = var.ami_amazon_linux
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = [var.security_group_id]
+  ami                    = var.ami_amazon_linux
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
 
   tags = {
     Name = "c8.local"
@@ -20,11 +20,11 @@ resource "aws_instance" "amazon_linux_vm" {
 }
 
 resource "aws_instance" "ubuntu_vm" {
-  ami                         = var.ami_ubuntu
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = [var.security_group_id]
+  ami                    = var.ami_ubuntu
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
 
   tags = {
     Name = "u21.local"
@@ -40,12 +40,11 @@ resource "local_file" "ansible_inventory" {
   filename = "${path.module}/hosts"
 
   content = <<-EOT
-   [frontend]
-c8.local ansible_host=107.20.81.70 ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/Ansible01.pem
+    [frontend]
+    c8.local ansible_host=${aws_instance.amazon_linux_vm.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/Ansible01.pem
 
-[backend]
-u21.local ansible_host=13.222.170.73 ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/Ansible01.pem
-
+    [backend]
+    u21.local ansible_host=${aws_instance.ubuntu_vm.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/Ansible01.pem
   EOT
 
   file_permission      = "0777"
