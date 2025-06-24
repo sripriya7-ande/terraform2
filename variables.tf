e0780c611d0adcb3919284b21335c0e5a8ce1906
nithinreddy8811 <nithinreddy8811@gmail.com>
2025-06-24 15:13:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,31 +1,14 @@
-variable "region" {
-  description = "AWS region"
-  default     = "us-east-1"
-}
-
 variable "vpc_id" {
   description = "Existing VPC ID"
+  default     = "vpc-094eeb2b40d546e07"
 }
 
 variable "subnet_id" {
   description = "Subnet ID inside the above VPC"
+  default     = "subnet-06e5859208fb840f5"
 }
 
 variable "key_name" {
-  description = "Ansible01"
-}
-
-variable "ami_amazon_linux" {
-  description = "AMI ID for Amazon Linux"
-  default     = "ami-0f3f13f145e66a0a3"
-}
-
-variable "ami_ubuntu" {
-  description = "AMI ID for Ubuntu"
-  default     = "ami-020cba7c55df1f615"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t2.micro"
+  description = "EC2 Key Pair Name"
+  default     = "Ansible01"
 }
