902600b00312cbcaf5fba6e1a683921b1d737fd0
nithinreddy8811 <nithinreddy8811@gmail.com>
2025-06-24 15:49:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "region" {
-  description = "AWS region to deploy instances"
+  description = "AWS region"
   default     = "us-east-1"
 }
 
 variable "vpc_id" {
-  description = "VPC ID"
+  description = "Existing VPC ID"
   default     = "vpc-0b23deca78dbcdfc7"
 }
 
@@ -13,14 +13,19 @@ variable "subnet_id" {
   default     = "subnet-028ebfc5aa672d8b6"
 }
 
-variable "key_name" {
-  description = "EC2 Key Pair Name"
-  default     = "Ansible01"
-}
-
 variable "security_group_id" {
   description = "Security Group ID"
   default     = "sg-00dd35d245995813f"
+}
+
+variable "key_name" {
+  description = "Key Pair name"
+  default     = "jenkins"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
 }
 
 variable "ami_amazon_linux" {
@@ -31,9 +36,4 @@ variable "ami_amazon_linux" {
 variable "ami_ubuntu" {
   description = "Ubuntu 24.04 AMI ID"
   default     = "ami-020cba7c55df1f615"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t2.micro"
 }
